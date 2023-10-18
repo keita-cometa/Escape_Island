84fc33089ca42b402c0b1bf019e60f1f26a908c9
松本彗汰 <h1202315010@hiratagakuen.onmicrosoft.com>
2023-10-18 17:59:36 +0900

    player の動きを変更

--- Assets/松本/PlayerController.cs
+++ Assets/松本/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     bool isMoving; 
 
     Vector2 input;
+    Transform player;
 
     //移動スピード
     [SerializeField] float moveSpeed;
@@ -15,18 +16,10 @@ public class PlayerController : MonoBehaviour
     //ほかのオブジェクトがないかの判定
     bool other_obj;
 
-    //X軸の移動範囲
-    [SerializeField] private float min_x = -8.0f;
-    [SerializeField] private float max_x = 8.0f;
-
-    //Y軸の移動範囲
-    [SerializeField] private float min_y = -4.5f;
-    [SerializeField] private float max_y = 4.5f;
-
     // Start is called before the first frame update
     void Start()
     {
-        
+        player= GameObject.Find ("Hero").transform;
     }
 
     // Update is called once per frame
@@ -35,6 +28,9 @@ public class PlayerController : MonoBehaviour
         // 移動中だと入力を受け付けない
         if (!isMoving)
         {
+            //判定の初期化
+            other_obj = false;
+
             // キーボードの入力情報をinputに格納
             input.x = Input.GetAxisRaw("Horizontal")*2.05f; // 横方向
             input.y = Input.GetAxisRaw("Vertical")*2.05f;  // 縦方向
@@ -46,9 +42,25 @@ public class PlayerController : MonoBehaviour
                 Vector2 targetPos = transform.position;
                 targetPos += input;
 
-                
-                if(!other_obj)
+                Vector2 target_v = new Vector2(targetPos.x, targetPos.y);
+                Vector2 Hero_pos = new Vector2(player.position.x, player.position.y);
+
+                //Rayの作成
+                Ray2D ray = new Ray2D(Hero_pos, target_v);
+                //Raycastの作成
+                RaycastHit2D hit= Physics2D.Raycast(ray.origin, ray.direction, 2.0f);
+
+                //Rayが当たった時判定をtrueにする
+                if (hit)
+                {
+                    other_obj = true;
+                }
+               
+                //ほかのコライダーがないとき移動を開始する
+                if (!other_obj)
                 StartCoroutine(Move(targetPos));
+
+                
             }
         }
     }
@@ -72,10 +84,6 @@ public class PlayerController : MonoBehaviour
         transform.position = targetPos;
         isMoving = false;
     }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        other_obj = true;
-    }
+  
 
 }
