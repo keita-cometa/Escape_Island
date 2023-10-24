90b0d1762f106203c094f2fbc88b742922bce64e
松本彗汰 <h1202315010@hiratagakuen.onmicrosoft.com>
2023-10-24 12:40:53 +0900

    UI変更

--- Assets/松本/Main1/Battle/CountDown.cs
+++ Assets/松本/Main1/Battle/CountDown.cs
@@ -2,29 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class CountDown : MonoBehaviour
 {
-    public float CountDownTime;//カウントダウン変数
-   
+    StartC startc;//StartC
+
+    //カウントダウン変数
+    public float countdown;
+
+    //テキスト変数
+    public Text timeText;
+
+    //読み込むシーン名
+    public string sceneName;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        startc = GetComponent<StartC>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
-        //経過時間を引いていく
-        CountDownTime -= Time.deltaTime;
-
-        //0秒以下になったら固定
-        if(CountDownTime<=0.0f)
+        if (StartC.onclick)
         {
-            CountDownTime = 0.0f;
+            //カウントを減らす処理
+            countdown -= Time.deltaTime;
+
+            //画面に表示する
+            timeText.text = countdown.ToString("f1") + "秒";
+
+            if (countdown <= 0)
+            {
+                //カウントが0以下になったらLoadを呼び出してシーン移行
+                Load();
+            }
         }
+    }
+
+    public void Load()
+    {
+        SceneManager.LoadScene(sceneName);
     }
 }
--- Assets/松本/Main1/Battle/StartC.cs
+++ Assets/松本/Main1/Battle/StartC.cs
@@ -1,25 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StartC : MonoBehaviour
 {
-    public bool gameplay;
+    public static bool onclick;//クリックされたかどうか判別変数
+
+    public GameObject StartBotton;//スタートボタン
+    public GameObject StartTimeText;//スタートタイムテキスト
+
+    int time=3;//開始までの時間
+    public Text timetext;//開始までの時間表示
+
+    int gametime = 3;
+    int times;
 
     // Start is called before the first frame update
     void Start()
     {
-        gameplay = false;
+        StartTimeText.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(onclick)
+        {
+            times += (int)Time.deltaTime;
+            time = gametime - times;
+            timetext.text = time.ToString();
+        }
+     
     }
 
     public void Load()
     {
-        gameplay = true;
+        Invoke("Click", 3.0f);
+     
+        for(int i=0;i<=3;i++)
+        {
+            StartTimeText.SetActive(true);
+        }
+        
+        StartBotton.SetActive(false);//ボタン非表示
+
+    }
+
+    public void Click()
+    {
+        onclick = true;
+ 
     }
 }
