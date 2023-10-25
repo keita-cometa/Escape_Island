492fb2b76dda38b0125f9db5b687a77508a45af5
松本彗汰 <h1202315010@hiratagakuen.onmicrosoft.com>
2023-10-25 12:14:32 +0900

    バトルUI完成

---
+++ Assets/松本/Main1/Battle/GoalManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GoalManager : MonoBehaviour
+{
+    public int goalnum;//クリアに必要な個数
+    public string sceneName;//読み込むシーン名
+
+    public Text Goaltext;//クリア個数テキスト
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Goaltext = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //目標個数表示
+        Goaltext.text = goalnum.ToString("0");
+
+        //goalnumが0になったらクリア関数を呼び出す
+        if(goalnum==0)
+        {
+            Clear();
+        }
+    }
+
+    //クリアしたときの関数
+    void Clear()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
--- Assets/松本/Main1/Battle/StartC.cs
+++ Assets/松本/Main1/Battle/StartC.cs
@@ -6,43 +6,33 @@ using UnityEngine.UI;
 public class StartC : MonoBehaviour
 {
     public static bool onclick;//クリックされたかどうか判別変数
+    public static bool starttf;
 
     public GameObject StartBotton;//スタートボタン
     public GameObject StartTimeText;//スタートタイムテキスト
 
-    int time=3;//開始までの時間
-    public Text timetext;//開始までの時間表示
-
-    int gametime = 3;
-    int times;
 
     // Start is called before the first frame update
     void Start()
     {
         StartTimeText.SetActive(false);
+        starttf = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(onclick)
-        {
-            times += (int)Time.deltaTime;
-            time = gametime - times;
-            timetext.text = time.ToString();
-        }
-     
+
     }
 
     public void Load()
     {
         Invoke("Click", 3.0f);
-     
-        for(int i=0;i<=3;i++)
-        {
-            StartTimeText.SetActive(true);
-        }
-        
+        starttf = true;
+
+        Debug.Log("aaa");
+        StartTimeText.SetActive(true);//開始時間表示
+              
         StartBotton.SetActive(false);//ボタン非表示
 
     }
