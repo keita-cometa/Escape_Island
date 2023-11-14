0e58ac849912a0be59eb037b265fe253bb6772a9
松本彗汰 <h1202315010@hiratagakuen.onmicrosoft.com>
2023-11-14 12:32:33 +0900

    ゲームクリアシーン完成

---
+++ Assets/松本/GameClear/UImanager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UImanager : MonoBehaviour
+{
+    public GameObject GameClear;
+    public GameObject atogaki;
+    public GameObject titleButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameClear.SetActive(true);
+        atogaki.SetActive(false);
+        titleButton.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Invoke("next", 3.0f);
+    }
+
+    void next()
+    {
+        GameClear.SetActive(false);
+        atogaki.SetActive(true);
+        Invoke("End", 3.0f);
+    }
+
+    void End()
+    {
+        titleButton.SetActive(true);
+    }
+}
