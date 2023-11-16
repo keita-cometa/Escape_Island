3aa788304bfacf3f7561a7f6109848f0c3657925
松本彗汰 <h1202315010@hiratagakuen.onmicrosoft.com>
2023-11-16 12:33:33 +0900

    シーン５大体完成

---
+++ Assets/松本/Main5/NextArea.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class NextArea : MonoBehaviour
+{
+    public GameObject NextButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NextButton.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
