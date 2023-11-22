5c7b7cafa74569218efefd2a6fc27f1b43620153
北田夢佐志 <h1202319006@hiratagakuen.onmicrosoft.com>
2023-11-22 10:58:26 +0900

    蛇とイノシシの動き

--- Assets/北田/SnakeBattle/Left.cs
+++ Assets/北田/SnakeBattle/Left.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SnakeController : MonoBehaviour
+public class Left : MonoBehaviour
 {
+   
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,10 @@ public class SnakeController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        transform.position = new Vector3(11, 2, 0);
     }
 }
