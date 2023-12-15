41d974a20fd2b47906b36e782b90c791b284d288
松本彗汰 <h1202315010@hiratagakuen.onmicrosoft.com>
2023-12-15 11:17:21 +0900

    メインシーンキーボード操作でできるように完成

--- Assets/Script/Hellmode.cs
+++ Assets/Script/Hellmode.cs
@@ -1,22 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Hellmode : MonoBehaviour
 {
+    //public GameObject HellButton;
     // Start is called before the first frame update
+
+    public GameObject text;
+    public GameObject NormalButton;
+    private AudioSource audioSource;
+    [SerializeField] private AudioClip se;
     void Start()
     {
-        
+        //Color Bcolor = HellButton.GetComponent<Renderer>().material.color;
+        //Bcolor.a = 0.05f;
+        //HellButton.GetComponent<Renderer>().material.color = Bcolor;
+        audioSource = GetComponent<AudioSource>();
+        text.SetActive(false);
+        NormalButton.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(GameManager.ClearFlg)
+        {
+            //Color Bcolor = HellButton.GetComponent<Text>().material.color;
+            //Bcolor.a = 1f;
+            //HellButton.GetComponent<Renderer>().material.color = Bcolor;
+        }
     }
     public void Click()
     {
         GameManager.Hellmode = true;
+        audioSource.PlayOneShot(se);
+        text.SetActive(true);
+        NormalButton.SetActive(true);
     }
 }
